06791d0a5a3b673a0145fbe1dd10e9da36233b1e
JuanpaVJ <juanpavj1954@gmail.com>
2023-07-14 00:46:30 -0600

    needed docs

--- tesseract-backend/index.js
+++ tesseract-backend/index.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const app = express();
+const routes = require("./routes/routes.js");
+const config = require("./config.js");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.use("/apis", routes);
+
+//!Listening
+app.listen(config.server_port, () =>
+  console.log(`Server running in port ${config.server_port}`)
+);
